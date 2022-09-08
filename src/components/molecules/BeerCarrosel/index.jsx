43fea7e0a031b78b19e3fa0a540a31ae91481049
jonathan <jonathan13pvc@gmail.com>
2022-09-08 16:47:59 -0500

    finish

--- src/components/molecules/BeerCarrosel/index.jsx
+++ src/components/molecules/BeerCarrosel/index.jsx
@@ -1,43 +1,35 @@
+import "./styles.scss";
 
-import "./styles.scss"
-
-import React from 'react'
-import imgmain from '../../../assets/img/Coupleofbeers.png'
-
-import imgbeer from '../../../assets/img/Trigobeer.png'
-
+import React from "react";
+import imgmain from "../../../assets/img/Coupleofbeers.png";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import imgbeer from "../../../assets/img/Trigobeer.png";
+import poker from "../../../assets/img/Pokerbeer.png";
 
 const BeerCarrosel = () => {
   return (
-    
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" >
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src={imgmain}class="d-block" alt="2" />    
-      </div>
-      <div class="carousel-item">
-        <img src={imgmain} class="d-block" alt="4" />
-      </div>
-      <div class="carousel-item">
-        <img src={imgmain} class="d-block" alt="6"/>
-      </div>
+    <div
+      id="carouselExampleIndicators"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <Carousel className="container_carrosel" showThumbs={false} autoPlay>
+        <div>
+          <img src={imgbeer} alt="" />
+          <p className="legend">Club Colombia</p>
+        </div>
+        <div>
+          <img src={imgmain}  alt=""/>
+          <p className="legend">Budweiser</p>
+        </div>
+        <div>
+          <img src={poker} alt=""/>
+          <p className="legend">Poker</p>
+        </div>
+      </Carousel>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-    
-  )
-}
+  );
+};
 
-export default BeerCarrosel
+export default BeerCarrosel;
